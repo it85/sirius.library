f8f0a8759da3611fdb61646e6519df5c0b817561
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-27 23:18:24 -0500

    added TimeStamp to RealtimeQuote; added CandleStick creation logic in Builder

--- src/main/java/global/Builder.java
+++ src/main/java/global/Builder.java
@@ -1,7 +1,10 @@
 package global;
 
 import java.sql.Timestamp;
+import java.util.Deque;
+
 import model.CandleStick;
+import model.RealTimeQuote;
 
 public class Builder {
 	
@@ -16,5 +19,45 @@ public class Builder {
 		
 		return new CandleStick(timeStamp, open, high, low, close, volume);
 	}
+	
+	public static CandleStick createCandleStick(Deque<RealTimeQuote> quotes){
+		
+		Timestamp timeStamp = null;
+		
+		double open = quotes.getFirst().getPrice();
+		double close = quotes.getLast().getPrice();
+		double volume = quotes.getLast().getVolume() - quotes.getFirst().getVolume();
+		
+		double high = -1;
+		for(RealTimeQuote quote : quotes){
+			if(quote.getPrice() > high){
+				high = quote.getPrice();
+			}
+		}
+		
+		double low = 99999999;
+		for(RealTimeQuote quote : quotes){
+			if(quote.getPrice() < low){
+				low = quote.getPrice();
+			}
+		}
+		
+		return new CandleStick(timeStamp, open, high, low, close, volume);
+	}
+	
+	public static RealTimeQuote createRealTimeQuote(String[] arr) throws NumberFormatException, ArrayIndexOutOfBoundsException{
+		
+//		Timestamp timeStamp = new Timestamp(Long.parseLong(arr[0]));
+//		double price = Double.parseDouble(arr[1]);
+//		double volume = Double.parseDouble(arr[2]);
+//		
+//		return new RealTimeQuote(timeStamp, price, volume);
+		
+		double price = Double.parseDouble(arr[0]);
+		double volume = Double.parseDouble(arr[1]);
+		
+		return new RealTimeQuote(price, volume);
+	}
+
 
 }
--- src/main/java/model/RealTimeQuote.java
+++ src/main/java/model/RealTimeQuote.java
@@ -1,12 +1,33 @@
 package model;
 
+import java.sql.Timestamp;
+
 public class RealTimeQuote {
 	
-	public double price;
-	public double volume;
+	private double price;
+	private double volume;
+	private  Timestamp timeStamp;
 	
 	public RealTimeQuote(double price, double volume){
 		this.price = price;
 		this.volume = volume;
+	}	
+	
+	public RealTimeQuote(Timestamp timeStamp, double price, double volume){
+		this.timeStamp = timeStamp;
+		this.price = price;
+		this.volume = volume;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+	public Timestamp getTimeStamp() {
+		return timeStamp;
 	}
 }
