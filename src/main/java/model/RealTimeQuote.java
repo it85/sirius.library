1596a168f035ab343ebd8b5dbf351494a6537562
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-27 21:20:52 -0500

    created new class RealTimeQuote

---
+++ src/main/java/model/RealTimeQuote.java
@@ -0,0 +1,12 @@
+package model;
+
+public class RealTimeQuote {
+	
+	public double price;
+	public double volume;
+	
+	public RealTimeQuote(double price, double volume){
+		this.price = price;
+		this.volume = volume;
+	}
+}
